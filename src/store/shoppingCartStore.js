cfd7d6a5a9fc0d5e643b303de85059ae3770e9d8
Diana Rios <dianariosram@gmail.com>
2023-06-12 16:48:00 -0600

    added persist middleware to zustand

--- src/store/shoppingCartStore.js
+++ src/store/shoppingCartStore.js
@@ -1,73 +1,82 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useShoppingCartStore = create( ( set ) => ({
-    productsCart: {},
-    // add to cart
-    addToCart: ( product ) => set( ( state ) => {
-        const productId = product.id;
-  
-        if ( state.productsCart.hasOwnProperty( productId ) ) {
-            return {
-                productsCart: {
-                    ...state.productsCart,
-                    [productId] : {
-                        ...state.productsCart[productId],
-                        count: state.productsCart[productId].count + 1,
-                    },
+export const useShoppingCartStore = create( 
+    persist(
+        ( set ) => ({
+            productsCart: {},
+            // add to cart
+            addToCart: ( product ) => set( ( state ) => {
+                const productId = product.id;
+          
+                if ( state.productsCart.hasOwnProperty( productId ) ) {
+                    return {
+                        productsCart: {
+                            ...state.productsCart,
+                            [productId] : {
+                                ...state.productsCart[productId],
+                                count: state.productsCart[productId].count + 1,
+                            },
+                        }
+                    };
+                } else {
+                    return {
+                        productsCart: {
+                            ...state.productsCart,
+                            [productId]: {
+                                productData: product,
+                                count: 1,
+                            },
+                        }
+                    };
                 }
-            };
-        } else {
-            return {
-                productsCart: {
-                    ...state.productsCart,
-                    [productId]: {
-                        productData: product,
-                        count: 1,
-                    },
+            }),
+            // Buttons to add and remove
+            removeProduct: ( id ) => set( (state) => {
+                if(state.productsCart[id].count > 1) {
+                    return {
+                        productsCart: {
+                            ...state.productsCart,
+                            [id]: {
+                                ...state.productsCart[id],
+                                count: state.productsCart[id].count - 1
+                            }
+                        }
+                    }
+                } else {
+                    const updatedProductsCart = { ...state.productsCart };
+                    delete updatedProductsCart[id];
+            
+                    return {
+                    productsCart: updatedProductsCart
+                    }
                 }
-            };
-        }
-    }),
-    // Buttons to add and remove
-    removeProduct: ( id ) => set( (state) => {
-        if(state.productsCart[id].count > 1) {
-            return {
+            }),
+        
+            addProduct: ( id ) => set( (state) => ({
                 productsCart: {
                     ...state.productsCart,
                     [id]: {
                         ...state.productsCart[id],
-                        count: state.productsCart[id].count - 1
+                        count: state.productsCart[id].count + 1
                     }
                 }
-            }
-        } else {
-            const updatedProductsCart = { ...state.productsCart };
-            delete updatedProductsCart[id];
-    
-            return {
-            productsCart: updatedProductsCart
-            }
-        }
-    }),
-
-    addProduct: ( id ) => set( (state) => ({
-        productsCart: {
-            ...state.productsCart,
-            [id]: {
-                ...state.productsCart[id],
-                count: state.productsCart[id].count + 1
-            }
-        }
-    })),
-
-    // Remove to cart
-    removeToCart: ( id ) => set( (state) => {
-        const updatedProductsCart = { ...state.productsCart };
-        delete updatedProductsCart[id];
-
-        return {
-            productsCart: updatedProductsCart
-        }
-    })
-}));
+            })),
+        
+            // Remove to cart
+            removeToCart: ( id ) => set( (state) => {
+                const updatedProductsCart = { ...state.productsCart };
+                delete updatedProductsCart[id];
+        
+                return {
+                    productsCart: updatedProductsCart
+                }
+            })
+        }),
+        {
+            name: 'shopping-cart-store', 
+            getStorage: () => sessionStorage, 
+        } 
+    )
+);
   
--- src/store/user.js
+++ src/store/user.js
@@ -1,14 +1,29 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useUserStore = create( set => ({
-    name: "",
-    email: "",
-    city: "",
-    state: "",
-    userDataLogged: ( user ) => set( () => ({
-        name: user.name,
-        email: user.email,
-        city: user.city,
-        state: user.state
-    }))
-}))
+export const useUserStore = create( 
+    persist(
+        (set) => ({
+            name: "",
+            email: "",
+            city: "",
+            state: "",
+            userDataLogged: ( user ) => set( () => ({
+                name: user.name,
+                email: user.email,
+                city: user.city,
+                state: user.state
+            })),
+            userUnlogged: ( user ) => set( () => ({
+                name: "",
+                email: "",
+                city: "",
+                state: ""
+            })),
+        }),
+        {
+            name: 'userLogged', 
+            getStorage: () => localStorage,
+        }
+    )
+)
